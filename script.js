6e4d882050e97c07cc0955fe43100026def7cfe8
Noriel Dimaano <norieldimaano@gmail.com>
2023-09-22 05:12:15 +0800

    init

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js"
 
 
 const appSettings = {
@@ -17,5 +17,43 @@ const todoListEl = document.getElementById("todo-list")
 listBtnEl.addEventListener("click", function(){
     let listValue = inputFieldEl.value
     push(todoListInDb, listValue)
-    todoListEl.innerHTML += `<li> ${listValue} </li>`
+    clearInputField()
+    
 })
+
+onValue(todoListInDb, function(snapshot){
+    let itemArray = Object.entries(snapshot.val())
+
+    todoListEl.innerHTML = ""
+
+    for(let i=0; i < itemArray.length; i++){
+        let currentItem = itemArray[i]
+        let currentitemId = currentItem[0]
+        let currentitemValue = currentItem[1]
+
+        inputValueAppear(currentItem)
+    }
+})
+
+function clearTodoListEl(){
+    todoListEl.innerHTML = ""
+}
+function clearInputField(){
+    inputFieldEl.value = ""
+}
+
+function inputValueAppear(item){
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+
+    newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function(){
+        let locationDatabase = ref(database, `todoList/${itemID}`) 
+        remove(locationDatabase)
+    })
+
+    todoListEl.append(newEl)
+}
